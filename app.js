6aa657c1a45cd3857c206a428f4eee5670d6c3bf
minhquanghaiquan <minhquanghaiquan@gmail.com>
2020-06-19 14:47:31 +0200

    second commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors())
 app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
-// app.use(require('./routes/user'))
+app.use(require('./routes/user'))
 
 
 // if(process.env.NODE_ENV=="production"){
